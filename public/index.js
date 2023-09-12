e7c800c08441d8fbe32e28fc7fc6d880060f5dbd
Efemena Festus <festusmena9@gmail.com>
2023-09-12 15:41:15 +0000

    added more logos

--- public/index.js
+++ public/index.js
@@ -2,5 +2,9 @@ import Logo from './logo.png'
 import HeroPic from './heropic.png'
 import Ui from './ui.png'
 import OneTap from './onetap.png'
+import UnibenMfb from './unibenMfb.png'
+import Vector2 from './vector2.png'
+import Subtract from './subtract.png'
+import WemaBank from './wemabank.png'
 
-export {Logo, HeroPic, Ui, OneTap }
+export {Logo, HeroPic, Ui, OneTap, UnibenMfb, Vector2, Subtract, WemaBank}
--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -1,7 +1,10 @@
 export default function Testimonials() {
     return (
-     <div className="bg-[#000212] absolute w-full h-96">
-      
+     <div className="bg-white relative w-full h-96">
+      <div className="w-1/2 h-full absolute top-0 left-0">
+        <div>Don't Take Our Word For IT</div>
+        <div>trust our customers</div>
+      </div>
      </div>
     );
   }
