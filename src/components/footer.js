534d5329d581de25dcd8891f7774dc17a781e18b
Efemena Festus <festusmena9@gmail.com>
2023-09-12 14:06:12 +0000

    latest updates

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@
 
 export default function Footer() {
     return (
-     <div className="bg-[#000212] absolute w-full h-72">
+     <div className="bg-[#000212] absolute w-full h-96">
       
      </div>
     );
--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -1,4 +1,4 @@
-export default function Sensors() {
+export default function Testimonials() {
     return (
      <div>Motion sensors...</div>
     );
