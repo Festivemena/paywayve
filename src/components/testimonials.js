3f3545d9bf660d3b7ef2a7777e17551158132986
Efemena Festus <festusmena9@gmail.com>
2023-09-13 05:52:42 +0000

    Finally Testimonials

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -5,7 +5,7 @@ function Individual() {
   return (
     <div className=" shadow-3xl -mt-[530px] justify-between items-center flex z-20  absolute">
       <div className="px-20 mr-10 flex">
-      <div className="w-1/2 p-4  shadow-2xl rounded-lg ">
+      <div className="w-1/2 p-4 mr-5  shadow-2xl rounded-lg ">
         <div className="w-full flex">
           <Image className="rounded-full" alt="Ijeoma" src={Ijeoma} />
           <div className="flex-1 pl-6 py-8 ">
@@ -13,7 +13,7 @@ function Individual() {
             <div className="text-[18px]">News caster</div>
           </div>
         </div>
-        <div className="text-[14px] text-left px-6 pt-8">
+        <div className="text-[14px] bg-[#F2FCFF] py-2 text-left px-6 mt-8">
         Using the Paywayve Card has truly transformed the way I handle my daily transactions. Whether it's making payments with a simple tap, marking attendance effortlessly, or paying for rides within the Payway partner ecosystem, this card has become an indispensable part of my life. The security measures in place also give me peace of mind, and it's such a relief to know that my transactions are protected. Plus, the fact that it's widely accepted at partner locations makes it incredibly convenient. I also appreciate the eco-friendly aspect of going paperless. It's a game-changer, and I couldn't be happier with the Payway Card – it's the future of fintech!
         </div>
         <div className="px-6 pt-8">22 Feb 2023 . 8:15 AM</div>
@@ -27,7 +27,7 @@ function Individual() {
             <div className="text-[18px]">Chemical Engineer</div>
           </div>
         </div>
-        <div className="text-[14px] text-left px-6 pt-8">
+        <div className="text-[14px] bg-[#F2FCFF] py-2 text-left px-6 mt-8">
         Paywayve Card simplifies my life – tap for payments, attendance, and ride payments. Secure, accepted everywhere, and eco-friendly. It's a game-changer!
         </div>
         <div className="px-6 pt-8">22 Feb 2023 . 8:15 AM</div>
@@ -40,7 +40,7 @@ function Individual() {
             <div className="text-[18px]">Student</div>
           </div>
         </div>
-        <div className="text-[14px] text-left px-6 pt-8">
+        <div className="text-[14px] bg-[#F2FCFF] py-2 text-left px-6 mt-8">
         Paywayve Card has revolutionized my transac...
         </div>
         <div className="px-6 pt-8">22 Feb 2023 . 8:15 AM</div>
@@ -64,6 +64,7 @@ export default function Testimonials() {
       </div>
       </div>
       <Individual />
+      <div className="text-[16px] font-bold mt-16 pl-12">All testimonial posts...</div>
      </div>
     );
   }
