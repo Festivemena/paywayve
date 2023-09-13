19099311b15387ab52cded8c9908d88083f55453
Efemena Festus <festusmena9@gmail.com>
2023-09-13 07:18:17 +0000

    modified:   src/components/footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,27 +10,33 @@ export default function Footer() {
         <Image alt="logo" src={Logo} />
       </div>
       <div className="flex-1 text-white justify-center items-center">
-        <div className="text-[20px]">Personal</div>
-        <div>Personal</div>
-        <div>Personal</div>
+        <div className="text-[20px] font-bold">Personal</div>
+        <div className="text-[16px] font-light">Discover</div>
+        <div className="text-[16px] font-light">Transfer and Spend</div>
+        <div className="text-[16px] font-light">Piggy Save</div>
+        <div className="text-[16px] font-light">Buy Airtime</div>
+        <div className="text-[16px] font-light">Buy Data</div>
+        <div className="text-[16px] font-light">Get Card</div>
       </div>
       <div className="flex-1 text-white justify-center items-center">
-      <div className="text-[20px]">Business</div>
-        <div>Personal</div>
-        <div>Personal</div>
-        <div>Personal</div>
-        <div>Personal</div>
+      <div className="text-[20px] font-bold">Business</div>
+        <div className="text-[16px] font-light">Discover</div>
+        <div className="text-[16px] font-light">Business Integration</div>
+        <div className="text-[16px] font-light">One Tap Device</div>
+        <div className="text-[16px] font-light">Inventory</div>
       </div>
       <div className="flex-1 text-white justify-center items-center">
-      <div className="text-[20px]">Company</div>
-        <div>Personal</div>
-        <div>Personal</div>
+      <div className="text-[20px] font-bold">Company</div>
+        <div className="text-[16px] font-light">Blog</div>
+        <div className="text-[16px] font-light">Press & Recognition</div>
+        <div className="text-[16px] font-light">Join Our Team</div>
+        <div className="text-[16px] font-light">About Us</div>
       </div>
       <div className="flex-1 text-white justify-center items-center">
-      <div className="text-[20px]">Help</div>
-        <div>Personal</div>
-        <div>Personal</div>
-        <div>Personal</div>
+      <div className="text-[20px] font-bold">Help</div>
+        <div className="text-[16px] font-light">Personal</div>
+        <div className="text-[16px] font-light">Personal</div>
+        <div className="text-[16px] font-light">Personal</div>
       </div>
       </div>
      </div>
